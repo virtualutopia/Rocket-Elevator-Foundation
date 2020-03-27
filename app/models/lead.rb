963b733161626ca0f7475492303b80a9555b4c4f
groovy6879 <groovy6879@hotmail.com>
2020-03-26 21:06:34 -0400

    SendGrid is done and working

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,14 +1,2 @@
 class Lead < ActiveRecord::Base
-
-    after_create :send_email
-  def send_email
-      UserNotifierMailer.send_lead_email(self).deliver_now
-     
-  end
-
-  private
-
-  def lead_params
-    params.require(:lead).permit(:email, :full_name, :project_name)
-  end
 end
