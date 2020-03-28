a2aecb62c897b0f6f16da4f08a73a0d72421618f
FelixDallaire <felix.dr01@gmail.com>
2020-03-28 09:38:31 -0400

    phone changed

--- lib/watson/watson.rb
+++ lib/watson/watson.rb
@@ -1,28 +1,44 @@
-
-
 require "json"
 require "ibm_watson/authenticators"
 require "ibm_watson/text_to_speech_v1"
-include IBMWatson
+require 'http'
 
-authenticator = Authenticators::IamAuthenticator.new(
-  apikey: ENV['WATSON_API_KEY']
+
+# If you have username & password in your credentials use:
+authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+  apikey: ENV['ibm_key'],
+  username: "#{ENV['imb_username']}",
+  password: "#{ENV['imb_pass']}",
 )
 
-text_to_speech = TextToSpeechV1.new(
+# If you have username & password in your credentials use:
+text_to_speech = IBMWatson::TextToSpeechV1.new(
   authenticator: authenticator
 )
-text_to_speech.service_url = ENV['WATSON_URL']
+text_to_speech.service_url = "https://cloud.ibm.com/docs/resources?topic=resources-services_region#paas-services"
 
-File.new("hello_world.wav", "wb") do |audio_file|
-  response = text_to_speech.synthesize(
-    text: "Hello world",
-    accept: "audio/wav",
-    voice: "en-US_AllisonV3Voice"
-  ).result
+assistant = AssistantV1.new(
+  authenticator: "<authenticator>"
+  version: "2017-04-21"
+)
+
+response = assistant.headers(
+  "Custom-Header" => "add_default_headers"
+  ).list_workspaces
+
+p "Status: #{response.status}"
+p "Headers: #{response.headers}"
+p "Result: #{response.result}"
+
+voice = text_to_speech.get_voice(
+  voice: "en-US_AllisonVoice")
   
+puts JSON.pretty_generate(voice.result)
+File.open("welcome.mp4", "w+") do |audio_file|
+  response = text_to_speech.synthesize(
+    text: "Hello world!",
+    accept: "audio/mp4",
+    voice: "en-US_AllisonVoice"
+  ).result
   audio_file << response
 end
-
-
-
