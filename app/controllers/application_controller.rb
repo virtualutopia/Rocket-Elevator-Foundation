0c10aab59683b34346a8a7bcdd225345d6be7fc7
FelixDallaire <felix.dr01@gmail.com>
2020-03-29 12:25:59 -0400

    ibm watson finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,7 @@ class ApplicationController < ActionController::Base
     def set_current_user
       if session[:user_id]
         @current_user = User.find(session[:user_id])
+        
       end
     end
   end
--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -1,39 +1,61 @@
+# frozen_string_literal: true
+require "json"
+require "ibm_watson/authenticators"
+require "ibm_watson/text_to_speech_v1"
+
 class WatsonController < ApplicationController
     before_action :watson
-    
-    def watson
-            # frozen_string_literal: true
-            
-            require "json"
-            require "ibm_watson/authenticators"
-            require "ibm_watson/text_to_speech_v1"
-            
-            # If using IAM
-            authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
-                apikey: "#{ENV['ibm_key']}"
-            )
-            
-            # If you have username & password in your credentials use:
-            # authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
-                #   username: "username",
-                #   password: "password"
-                # )
-                
-                # If you have username & password in your credentials use:
-                text_to_speech = IBMWatson::TextToSpeechV1.new(
-                authenticator: authenticator
-            )
-            text_to_speech.service_url = "https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/8fbb899b-7e00-48f8-9814-73aaf74e934a"
-            
-            puts JSON.pretty_generate(text_to_speech.list_voices.result)
 
-            File.open("welcome.wav", "w+") do |audio_file|
-                response = text_to_speech.synthesize(
-                text: "Hello world #{xx} world Hello world Hello world Hello world Hello world Hello world Hello world Hello world !",
-                accept: "audio/wav",
-                voice: "en-US_AllisonVoice"
-                ).result
-                audio_file << response
-            end
+
+    def watson
+
+        # If using IAM
+        authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+          apikey: "3iVBPxSerBLNlIh3K4ovvIluPwhsX7UhB1jjwC91o5vu"
+        )
+
+        # If you have username & password in your credentials use:
+        # authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+        #   username: "{username}",
+        #   password: "{password}"
+        # )
+
+        # If you have username & password in your credentials use:
+        text_to_speech = IBMWatson::TextToSpeechV1.new(
+          authenticator: authenticator
+        )
+        text_to_speech.service_url = "https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/8fbb899b-7e00-48f8-9814-73aaf74e934a"
+
+        # puts JSON.pretty_generate(text_to_speech.list_voices.result)
+
+
+        # VARAIBLES
+        name = current_user.firstName
+        nb_elevator = Elevator.count
+        nb_building = Building.count
+        nb_customer = Customer.count
+        nb_inactive_elevator = Elevator.where(status: 'Inactive').count
+        nb_quote = Quote.count
+        nb_leads = Lead.count
+        nb_battery = Battery.count
+        city_list = []
+        Address.all.each do |address|
+            city = address.city
+            city_list.push(city)
+        end
+        nb_city = city_list.length
+        # /VARIABLES
+
+
+        File.open("public/output.wav", "wb") do |audio_file|
+          response = text_to_speech.synthesize(
+            # text: "Hello world! #{elevator}",
+            text: "Welcome #{name}! There are currently #{nb_elevator} elevators deployed in the #{nb_building} buildings of your #{nb_customer} customers. Currently, #{nb_inactive_elevator} elevators are not in Running Status and are being serviced. Moreover, You currently have #{nb_quote} quotes awaiting processing and You currently have #{nb_leads} leads in your contact requests. Finally, there is #{nb_battery} Batteries are deployed across #{nb_city} cities.",
+            accept: "audio/wav",
+            voice: "en-US_AllisonVoice"
+          ).result
+          audio_file << response
+        end
+
     end
 end
