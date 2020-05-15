a07f17c99ed48cab6fd91ba0a96e502586c3373e
Farid A <farid.anooshehpour@gmail.com>
2020-05-15 08:11:30 -0400

    New IP from NEW instance of lightSali server after being locked out of previous server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "35.183.19.44", user: "ubuntu", roles: %w{app db web}
+server "35.183.65.134", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
@@ -25,9 +25,9 @@ set :linked_files, %w{config/database.yml config/application.yml config/newrelic
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{ubuntu@35.183.19.44}
-role :web, %w{ubuntu@35.183.19.44}
-role :db,  %w{ubuntu@35.183.19.44}
+role :app, %w{ubuntu@35.183.65.134}
+role :web, %w{ubuntu@35.183.65.134}
+role :db,  %w{ubuntu@35.183.65.134}
 
 
 
